9f35986ea53c255913645f2217994085048cf953
pushkarasharma <pushkarasharma11@gmail.com>
2021-06-23 19:39:59 +0530

    added login and register with redirect if authenticated

--- ToDo_List/base/urls.py
+++ ToDo_List/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetails, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetails, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterUser
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('create-task', TaskCreate.as_view(), name='create-task'),
